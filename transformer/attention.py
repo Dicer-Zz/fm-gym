fix: attention mask

transformer/attention.py
@@ -104,11 +104,14 @@ class MultiHeadAttention(nn.Module):
 
         attention_scores = attention_scores / math.sqrt(self.attention_head_size)
 
-        # print(attention_scores.shape, attention_mask.shape)
-        # # why?
-        # if attention_mask is not None:
-        #     attention_scores = attention_scores + attention_mask
-
+        if attention_mask is not None:
+            # attentin_scores: [batch_size, num_attention_heads, seq_length, seq_length]
+            # attention_mask: [batch_size, seq_length]
+            # extended_attention_mask: [batch_size, 1, 1, seq_length]
+            extend_attention_mask = attention_mask[:, None, None, :]
+            extend_attention_mask = (1.0 - extend_attention_mask) * -10000.0
+            attention_scores = attention_scores + extend_attention_mask
+        
         # Normalize
         attention_probs = nn.functional.softmax(attention_scores, dim=-1)
         attention_probs = self.dropout(attention_probs)

transformer/bert_model.py
@@ -7,6 +7,20 @@ from embeddings import BERTEmbeddings
 from xformer_encoder import XformerEncoder
 from prediction import BERTMLMHead, BERTNSPHead
 
+class BertPooler(nn.Module):
+    def __init__(self, hidden_size):
+        super().__init__()
+        self.dense = nn.Linear(hidden_size, hidden_size)
+        self.activation = nn.Tanh()
+
+    def forward(self, hidden_states):
+        # We "pool" the model by simply taking the hidden state corresponding
+        # to the first token.
+        # first_token_tensor = hidden_states[:, 0]
+        first_token_tensor = hidden_states
+        pooled_output = self.dense(first_token_tensor)
+        pooled_output = self.activation(pooled_output)
+        return pooled_output
 
 class BertModel(nn.Module):
     """
@@ -91,11 +105,15 @@ class BertModel(nn.Module):
             intermediate_size,
             hidden_act,
         )
-        self.mlm_head = BERTMLMHead(
-            hidden_size, mlm_head_act, vocab_size, layer_norm_eps
-        )
-        self.nsp_head = BERTNSPHead(hidden_size, nsp_head_act, 2)
+        self.pooler = BertPooler(hidden_size)
+        # self.mlm_head = BERTMLMHead(
+        #     hidden_size, mlm_head_act, vocab_size, layer_norm_eps
+        # )
+        # self.nsp_head = BERTNSPHead(hidden_size, nsp_head_act, 2)
         self.initializer_range = initializer_range
+        
+        # Initialize all weights
+        self.apply(self._init_weights)
 
     def _init_weights(self, module):
         """Initialize the weights"""
@@ -165,9 +183,30 @@ class BertModel(nn.Module):
 
         embedding_output = self.embeddings(input_ids, token_type_ids, position_ids,)
         encoder_outputs = self.encoder(embedding_output, attention_mask,)
-        mlm_output = self.mlm_head(encoder_outputs,)
-        nsp_output = self.nsp_head(encoder_outputs)
-        return encoder_outputs, mlm_output, nsp_output
+        # encoder_outputs: [batch_size, seq_length, hidden_size]
+        # mlm_output = self.mlm_head(encoder_outputs)
+        # nsp_output = self.nsp_head(encoder_outputs[:, 0, :])
+
+        # total_loss = None
+        # if mlm_labels is not None and nsp_labels is not None:
+        #     loss_fn = nn.CrossEntropyLoss()
+        #     # mlm_output: [batch_size, seq_length, vocab_size]
+        #     # mlm_labels: [batch_size, seq_length]
+        #     mlm_loss = loss_fn(mlm_output.view(-1, mlm_output.size(-1)), mlm_labels.view(-1))
+        #     # nsp_output: [batch_size, 2]
+        #     # nsp_labels: [batch_size]
+        #     nsp_loss = loss_fn(nsp_output.view(-1, nsp_output.size(-1)), nsp_labels.view(-1))
+        #     total_loss = mlm_loss + nsp_loss
+
+        # return encoder_outputs, mlm_output, nsp_output
+        return {
+            # "loss": total_loss,
+            # "mlm_output": mlm_output,
+            # "nsp_output": nsp_output,
+            # "mlm_loss": mlm_loss,
+            # "nsp_loss": nsp_loss,
+            "encoder_outputs": encoder_outputs,
+        }
 
 if __name__ == "__main__":
     torch.manual_seed(42)
@@ -176,5 +215,5 @@ if __name__ == "__main__":
     input_ids = torch.randint(0, 30522, (10, 20))
 
     bert = BertModel()
-    hidden_states, mlm_output, nsp_output = bert(input_ids)
-    print(hidden_states, mlm_output.shape, nsp_output.shape)
+    outputs = bert(input_ids)
+    print(outputs)

transformer/testing.py
@@ -13,7 +13,7 @@ torch.manual_seed(SEED)
 torch.cuda.manual_seed(SEED)
 
 # Step 2: Load BERT hyperparameters
-from transformers import AutoConfig, AutoTokenizer
+from transformers import AutoConfig, AutoTokenizer, BertForPreTraining
 
 model_name = "bert-base-uncased"
 config = AutoConfig.from_pretrained(model_name)   # 'bart-base'
@@ -26,9 +26,10 @@ our_bert = BertModel()
 
 # Step 4: Get the model output without dropout
 our_bert.eval()
-inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
-last_hidden_states, _, _ = our_bert(**inputs)
-print(last_hidden_states)
+inputs = tokenizer(["Hello, my little dog is cute", "Hello, my cat is cute"], return_tensors="pt", padding=True)
+outputs = our_bert(**inputs)
+# print(last_hidden_states)
+print(outputs["encoder_outputs"])
 
 # Step 1: Set the Random Seed in the program entry 
 # IMPORTANT! SEED must be the same as ours!
@@ -46,7 +47,8 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)  # 'bart-base'
 
 # Step 3: Get the model output without dropout
 model.eval()
-inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+inputs = tokenizer(["Hello, my little dog is cute", "Hello, my cat is cute"], return_tensors="pt", padding=True)
 outputs = model(**inputs)
 last_hidden_states = outputs.last_hidden_state
 print(last_hidden_states)
+